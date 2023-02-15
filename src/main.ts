89c9a7804cd603fccd641764ff85ee5d29c9e854
hetrodoo <eduleite62@gmail.com>
2023-02-15 10:01:07 -0300

    Update main to get the port from the environment.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT || '3000'));
 }
 bootstrap().then(console.log).catch(console.error);
