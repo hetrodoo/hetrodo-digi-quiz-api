405193908cd2384c1178b0c9b0eef6f4a9f22bee
hetrodoo <eduleite62@gmail.com>
2023-02-15 09:59:46 -0300

    Update quiz controller to match the service response.

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { QuizService } from './quiz.service';
+import IQuiz from '../models/quiz/IQuiz';
 
 @Controller('quiz')
-export class QuizController {}
+export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
+
+  @Get()
+  public getQuiz(): IQuiz[] {
+    return this.quizService.getQuiz();
+  }
+}
