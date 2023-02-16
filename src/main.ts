5346159f7edf452cd58e73d1be7f4bdc49543374
hetrodoo <eduleite62@gmail.com>
2023-02-15 22:12:54 -0300

    Add cors options.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://digi-quiz.hetrodo.dev', 'http://localhost:8080'],
+  });
   await app.listen(parseInt(process.env.PORT || '3000'));
 }
 bootstrap().then(console.log).catch(console.error);
